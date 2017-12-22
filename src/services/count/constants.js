f3989b189ac521bff96c15ccdea02a390daa0542
Bisic <florian.levreau@laposte.net>
2017-12-22 22:56:47 +0100

    5
    5

--- src/services/count/constants.js
+++
@@ -1,4 +0,0 @@
-
-export const COUNT_INCREMENT = "@count - incrémenter la valeur du count";
-
-
--- src/services/todos/constants.js
+++
@@ -1,8 +0,0 @@
-
-export const COUNT_INCREMENT = "@count - incrémenter la valeur du count";
-
-export const TODOS_ADD_TODO = "@todos - add todo";
-
-export const REMOVE_TODO = "@todos - remove todo";
-
-export const REMOVE_LAST_TODO = "@todos - remove last todo";
